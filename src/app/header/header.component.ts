f540a5d267a63291d75c346b68c52aaae087b6f3
Aditya Patil <aditya@customtechnologies.biz>
2024-08-24 12:00:50 +0530

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -90,20 +90,20 @@ export class BannerComponent {
 
     this.responsiveOptions = [
       {
-        breakpoint: '1200px',
-        numVisible: 3,
-        numScroll: 3,
+        breakpoint: '2600px',   // max-width
+        numVisible: 1,    // the number of items items per page
+        numScroll: 1,   // the number of items items per page
       },
-      {
-        breakpoint: '1220px',
-        numVisible: 2,
-        numScroll: 2,
-      },
-      {
-        breakpoint: '1100px',
-        numVisible: 1,
-        numScroll: 1,
-      },
+      // {
+      //   breakpoint: '1220px',
+      //   numVisible: 2,
+      //   numScroll: 2,
+      // },
+      // {
+      //   breakpoint: '1100px',
+      //   numVisible: 1,
+      //   numScroll: 1,
+      // },
     ];
   }
 
