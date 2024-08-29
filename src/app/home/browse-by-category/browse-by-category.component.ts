fe369572383bf68324b091dbfe1690274ea42ced
Aditya Patil <aditya@customtechnologies.biz>
2024-08-29 09:03:32 +0530

    Added browse by category

--- src/app/home/browse-by-category/browse-by-category.component.ts
+++ src/app/home/browse-by-category/browse-by-category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-browse-by-category',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './browse-by-category.component.html',
   styleUrl: './browse-by-category.component.scss'
 })
