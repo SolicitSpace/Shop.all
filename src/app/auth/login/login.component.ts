760b2ba661da5bbbf749d2d70232886754c37491
Aditya Patil <patiladitya747@gmail.com>
2024-11-20 06:20:53 +0530

    Updated auth pages
    Segregated code and added navigations

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, InputTextModule, InputIconModule, IconFieldModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private navCtrl: Router) {}
 
+  navToHomepage() {
+    this.navCtrl.navigate(['home']);
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, InputTextModule, InputIconModule, IconFieldModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  constructor(private navCtrl: Router) {}
 
+  navToHomepage() {
+    this.navCtrl.navigate(['home']);
+  }
 }
